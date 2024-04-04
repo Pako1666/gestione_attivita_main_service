bb0fed27fb563de7fb7732cd06cafcf63028eeb0
Pako1666 <raffaele2003carrieri@gmail.com>
2024-04-04 09:53:23 +0200

    configurazione ambiente locale

--- gestore_attivita/src/main/java/it/gestore_attivita/gestore_attivita/GestoreAttivitaApplication.java
+++ gestore_attivita/src/main/java/it/gestore_attivita/gestore_attivita/GestoreAttivitaApplication.java
@@ -3,6 +3,9 @@ package it.gestore_attivita.gestore_attivita;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
+
 
 @SpringBootApplication
 public class GestoreAttivitaApplication {
